bd4718a98660ac2140e4d8cdb04dadb7ba554d56
Shiru99 <shriram.ghadge1999@gmail.com>
2021-07-17 19:28:33 +0530

    spring security with embedded (in-memory) database

--- spring-security-jdbc/src/main/java/io/fall/springsecurityjdbc/SecurityConfig.java
+++ spring-security-jdbc/src/main/java/io/fall/springsecurityjdbc/SecurityConfig.java
@@ -1,10 +1,14 @@
 package io.fall.springsecurityjdbc;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,13 +16,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired              // for embedded (in-memory) database
+    DataSource dataSource;  
 
     // 1. Configure Spring Security Authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("admin").password("1234").roles("ADMIN")
-            .and().withUser("user").password("1234").roles("USER");
+
+        // auth.jdbcAuthentication().dataSource(dataSource);
+
+        auth.jdbcAuthentication().dataSource(dataSource)
+            .withDefaultSchema()
+            .withUser(User.withUsername("admin").password("1234").roles("ADMIN"))
+            .withUser(User.withUsername("user").password("1234").roles("USER"));
     }
 
 
